c173bdf0ee97814c6e81856c36617ab36934a4e6
jiangys <jiangys@ruanyongjudeMacBook-Pro.local>
2015-06-22 13:16:40 +0800

    Initial Commit

---
+++ Weibo/UYViewController.m
@@ -0,0 +1,29 @@
+//
+//  UYViewController.m
+//  Weibo
+//
+//  Created by jiangys on 15/6/22.
+//  Copyright (c) 2015年 uxiaoyuan. All rights reserved.
+//
+
+#import "UYViewController.h"
+
+@interface UYViewController ()
+
+@end
+
+@implementation UYViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Weibo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Weibo
+//
+//  Created by jiangys on 15/6/22.
+//  Copyright (c) 2015年 uxiaoyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UYAppDelegate class]));
+    }
+}
